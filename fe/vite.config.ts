e030c48f99cd8dff7a859e986fe00a4b41d51b0d
duonghainam1 <namhai0301004@gmail.com>
2024-09-24 21:27:56 +0700

    vercel-1

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Đảm bảo các đường dẫn tĩnh được build đúng
+  base: './', // Sử dụng base để đường dẫn tĩnh đúng
+  build: {
+    outDir: 'dist', // Vercel yêu cầu thư mục build là dist
+  },
+  server: {
+    port: 3000, // Cấu hình cổng nếu cần khi chạy local
+  },
 });
