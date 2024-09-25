aab907d881f33617071383232f1de236d475c1b7
duonghainam1 <155941011+duonghainam1@users.noreply.github.com>
2024-09-25 14:48:45 +0700

    Update app.js

--- be/src/app.js
+++ be/src/app.js
@@ -1,10 +1,9 @@
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
 import morgan from "morgan";
 
-import { connectDB } from "./config/db";
-
+// Import các route khác nếu có
 import authRouter from "./routers/auth";
 import cartRouter from "./routers/cart";
 import productRouter from "./routers/product";
@@ -12,16 +11,13 @@ import categoryRouter from "./routers/category";
 import attributeRouter from "./routers/attribute";
 import Router_order from "./routers/order";
 
-
 const app = express();
 dotenv.config();
+
 // middleware
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
-
-// connect db
-connectDB(process.env.DB_URI);
 
 // routers
 app.use("/api/v1", authRouter);
@@ -29,6 +25,7 @@ app.use("/api/v1", productRouter);
 app.use("/api/v1", cartRouter);
 app.use("/api/v1", categoryRouter);
 app.use("/api/v1", attributeRouter);
-app.use("/api/v1", Router_order)
+app.use("/api/v1", Router_order);
 
-export const viteNodeApp = app;
+// Xuất app mà không cần gọi app.listen()
+export default app;
